bcf5b817e81a9f619dcc3cac74f104d3c66710c9
Gary Jones <gamajo@gamajo.com>
2015-04-27 15:54:59 +0100

    Update class-tgm-plugin-activation.php

--- class-tgm-plugin-activation.php
+++ class-tgm-plugin-activation.php
@@ -8,7 +8,7 @@
  * or theme author for support.
  *
  * @package   TGM-Plugin-Activation
- * @version   2.5.0-alpha
+ * @version   2.5.0-dev
  * @link      http://tgmpluginactivation.com/
  * @author    Thomas Griffin
  * @author    Gary Jones
